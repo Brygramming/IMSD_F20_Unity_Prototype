70ef159e1b57d4b387babf2463badb0227a0ea0a
Arash Izadi <arash.iz@live.com>
2020-09-27 16:32:35 -0400

    New Level

--- IMSD_F20_Unity_Prototype_One/Assets/Sphere/SphereMovement.cs
+++ IMSD_F20_Unity_Prototype_One/Assets/Sphere/SphereMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class SphereMovement : MonoBehaviour
 {
-	public float MovementAmt;
-	Vector3 StartingPosition;
-	Vector3 FinishingPosition;
+    public float MovementAmt;
+    Vector3 StartingPosition;
+    Vector3 FinishingPosition;
 
     //For Timed Enemy
     public GameObject TimedEnemy;
@@ -14,6 +14,17 @@ public class SphereMovement : MonoBehaviour
 
     public GameObject Enemies;
     public GameObject Goal;
+
+    //For New Level
+    private bool newGame;
+    public Transform player;
+    public Transform enemyOne;
+    public Transform enemyTwo;
+    public Transform enemyThree;
+    public Transform enemyFour;
+    public Transform enemyFive;
+    public Transform enemyCube;
+    private int levelCount = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +34,9 @@ public class SphereMovement : MonoBehaviour
         TimedEnemyPosition = TimedEnemy.transform.position;
 
         //Timed Enemy's Position
+
+        //New Level
+        newGame = true;
     }
 
     // Update is called once per frame
@@ -30,40 +44,71 @@ public class SphereMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-        	transform.position += new Vector3 (-MovementAmt * Time.deltaTime, 0, 0);
-        	Debug.Log("Left");
-        }else if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(-MovementAmt * Time.deltaTime, 0, 0);
+            Debug.Log("Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
-        	transform.position  += new Vector3 (+MovementAmt * Time.deltaTime, 0, 0);
-        	Debug.Log("Right");
+            transform.position += new Vector3(+MovementAmt * Time.deltaTime, 0, 0);
+            Debug.Log("Right");
         }
 
-        if(transform.position.x <= -10)
+        if (transform.position.x <= -10)
         {
-        	transform.position = StartingPosition;
-        } else if(transform.position.x >= 10)
+            transform.position = StartingPosition;
+        }
+        else if (transform.position.x >= 10)
         {
-        	transform.position = new Vector3(9, 0, 0);
+            transform.position = new Vector3(9, 0, 0);
         }
     }
 
     void OnTriggerEnter(Collider Tch)
     {
-    	if(Tch.gameObject.tag == "Enemy")
-    	{
-    		transform.position = StartingPosition;
-    		Debug.Log("Enemy touched");
-
+        if (Tch.gameObject.tag == "Enemy")
+        {
+            transform.position = StartingPosition;
+            Debug.Log("Enemy touched");
             //Timed Enemy
             TimedEnemy.transform.position = TimedEnemyPosition;
             Debug.Log("Timed Enemy Touched");
-    	} else if(Tch.gameObject.tag == "Win")
-    	{
-    		Destroy(Enemies);
-    		Destroy(Goal);
-            Destroy(TimedEnemy);
-    		transform.position = FinishingPosition;
-    		Debug.Log("You Win");
-    	}
+        }
+        else if (Tch.gameObject.tag == "Win")
+        {
+            //Destroy(Enemies);
+            //Destroy(Goal);
+            //Destroy(TimedEnemy);
+            transform.position = FinishingPosition;
+
+            NewLevel();
+            Debug.Log("Level: " + levelCount.ToString() + " - Next level!");
+        }
+    }
+    void NewLevel()     //A.I.
+    {
+        //Default startup pattern
+        player.position = new Vector3(-6.6f, 0);
+        enemyCube.position = new Vector3(-15, 1);
+        if (newGame)
+        {
+            enemyOne.position = new Vector3(-5, -3);
+            enemyTwo.position = new Vector3(0, -3);
+            enemyThree.position = new Vector3(5, -3);
+            enemyFour.position = new Vector3(-2.5f, -3);
+            enemyFive.position = new Vector3(2.5f, -3);
+            levelCount = 1;
+        }
+        else
+        {
+            //Random pattern
+            enemyOne.position = new Vector3(-5, Random.Range(-5f, 8f));
+            enemyTwo.position = new Vector3(0, Random.Range(-5f, 8f));
+            enemyThree.position = new Vector3(5, Random.Range(-5f, 8f));
+            enemyFour.position = new Vector3(-2.5f, Random.Range(-5f, 8f));
+            enemyFive.position = new Vector3(2.5f, Random.Range(-5f, 8f));
+            levelCount++;
+        }
+        newGame = false;
+
     }
 }
