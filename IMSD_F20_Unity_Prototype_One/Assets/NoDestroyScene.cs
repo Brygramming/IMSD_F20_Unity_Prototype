cc84fa9d9bee7a8a63c4cac1aa11311c66c9ca7b
Bryan Santiago <musicprostar11@gmail.com>
2020-09-30 13:36:03 -0400

    Bryan: Small Fix
    Fix the bug where the script duplicate the prefabs. It's now gone.

--- IMSD_F20_Unity_Prototype_One/Assets/NoDestroyScene.cs
+++ IMSD_F20_Unity_Prototype_One/Assets/NoDestroyScene.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class NoDestroyScene : MonoBehaviour
 {
+	private static NoDestroyScene iinstance;
+ 	void Awake()
+ 	{
+ 		
+         
+     	if(iinstance == null)
+     	{
+     		iinstance = this;
+     		DontDestroyOnLoad (this);
+     	} else
+     	{
+         	Destroy(gameObject);
+     	}
+ 	}
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,6 @@ public class NoDestroyScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    	DontDestroyOnLoad(this);
+    	
     }
 }
